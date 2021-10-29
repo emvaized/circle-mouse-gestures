8b3a4468710030662415f734b63c063da47184ae
Max <maximtsyba@gmail.com>
2021-10-30 01:20:48 +0300

    Background dimmer fixes

--- src/ui/circle/bg-dimmer.js
+++ src/ui/circle/bg-dimmer.js
@@ -1,12 +1,14 @@
 function showBackgroundDimmer() {
     backgroundDimmer = document.createElement('div');
     backgroundDimmer.setAttribute('id', 'cmg-page-dimmer');
+    backgroundDimmer.style.opacity = 0.0;
+    backgroundDimmer.style.transition = `opacity ${configs.animationDuration}ms ease-out`;
     // backgroundDimmer.setAttribute('style', `overflow: hidden; z-index: 9999; width:${document.documentElement.clientWidth}px; height: ${document.documentElement.clientHeight}px;  opacity: 0.0; transition: opacity ${configs.animationDuration}ms ease-out; position:fixed; background: black !important; top: 0px; left: 0px;`);
     document.body.appendChild(backgroundDimmer);
 
     setTimeout(function () {
         backgroundDimmer.style.opacity = configs.backgroundDimmerOpacity;
-    }, 1);
+    }, 3);
 }
 
 function hideBackgroundDimmer() {
