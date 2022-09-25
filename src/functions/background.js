203b9e4609ae6c67a583a5afa9fb6baa2a4d6393
Max <maximtsyba@gmail.com>
2022-09-26 01:04:31 +0300

    Make 'Open in popup window' size depend on screen size

--- src/functions/background.js
+++ src/functions/background.js
@@ -358,7 +358,8 @@ chrome.runtime.onMessage.addListener(
                                 });
                             }
 
-                            let height = 600, width = 500;
+                            // let height = 600, width = 500;
+                            let height = window.screen.height * 0.65, width = window.screen.height * 0.5;
                             let dx = request.dx - (width / 2), dy = request.dy - (height / 2);
 
                             /// check for screen overflow
