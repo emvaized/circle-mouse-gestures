2b2cf81bc5d3ca431855dc088b6687d27b4b9511
Max <maximtsyba@gmail.com>
2024-11-10 18:52:57 +0100

    Improvements for helper tooltip

--- src/ui/components/helper-tooltip.js
+++ src/ui/components/helper-tooltip.js
@@ -17,23 +17,9 @@ async function showLinkTooltip() {
     let linkToDisplay = hoveredLink.replaceAll('http://', '').replaceAll('https://', '').replaceAll('www.', '').trim();
     if (linkToDisplay[linkToDisplay.length - 1] == '/') linkToDisplay = linkToDisplay.slice(0, -1);
 
-    /// Set title
-    const label = document.createElement('span');
-    label.setAttribute('id', 'cmg-helper-tooltip-label');
-    label.innerText = typeOfMenu == 'linkMenu' ?
-        // configs.showLinkTextInTooltip && hoveredLinkTitle !== null && hoveredLinkTitle !== undefined && hoveredLinkTitle !== ''
-        //     && hoveredLinkTitle !== hoveredLink && hoveredLinkTitle !== linkToDisplay
-        //     ? (hoveredLinkTitle.length > 24 ? hoveredLinkTitle.substring(0, 24) + '...' : hoveredLinkTitle)
-        //     : 
-            chrome.i18n.getMessage('link')
-        : (typeOfMenu == 'imageMenu' || typeOfMenu == 'imageLinkMenu') ? chrome.i18n.getMessage('image')
-            : typeOfMenu == 'playerMenu' ? chrome.i18n.getMessage('player')
-                : typeOfMenu == 'selectionMenu' || (typeOfMenu == 'textFieldMenu' && textSelection.toString() !== '') ? chrome.i18n.getMessage('selectedText') :
-                    typeOfMenu == 'textFieldMenu' ? chrome.i18n.getMessage('inputField') : chrome.i18n.getMessage('page');
-
-
     /// Draw type icon
-    if (configs.showCategoryIconInTooltip) {
+    if (configs.showCategoryIconInTooltip) addTypeIcon(typeOfMenu, linkTooltip);
+    function addTypeIcon(typeOfMenu, linkTooltip){
         const typeIcon = document.createElement('div');
         typeIcon.setAttribute('style', `color: rgba(256,256,256,0.7); font-size: 12px !important;display: inline-block; margin-right: 3px;`);
 
@@ -64,66 +50,98 @@ async function showLinkTooltip() {
         linkTooltip.appendChild(typeIcon);
     }
 
-    label.innerHTML += '<br />';
-    label.style.color = `rgba(${fgColorRgb.red}, ${fgColorRgb.green}, ${fgColorRgb.blue}, 0.85)`;
-    linkTooltip.appendChild(label);
-
-    /// Set body
-    const text = document.createElement('span');
-    text.id = 'cmg-helper-tooltip-body';
-    const maxSymbolsInBody = 33;
-
-    if (typeOfMenu == 'imageMenu' || typeOfMenu == 'playerMenu' || typeOfMenu == 'imageLinkMenu') {
-        let fileName;
-        let link = typeOfMenu == 'imageLinkMenu' ? hoveredImageLink : hoveredLink;
-        try {
-            fileName = link.split('#').shift().split('?').shift().split('/').pop();
-        } catch (e) { if (configs.debugMode) console.log(e); }
-        text.innerText = fileName ?? link;
-    } else {
-        if (typeOfMenu == 'selectionMenu' || (typeOfMenu == 'textFieldMenu' && textSelection.toString() !== '')) {
-            const textSelectionString = textSelection.toString();
-            // text.innerText = textSelectionString;
-            text.innerText = `${textSelectionString.length} ${chrome.i18n.getMessage('symbolsCount').toLowerCase()}, ${textSelectionString.split(' ').length} ${chrome.i18n.getMessage('wordsCount').toLowerCase()}`;
-        } else if (typeOfMenu == 'textFieldMenu') {
-
-            /// Set tooltip body to display current clipboard content
-            const result = await getCurrentClipboard();
-            currentClipboardContent = result;
-
-            if (configs.debugMode) {
-                console.log('current clipboard content:');
-                console.log(currentClipboardContent);
-            }
-
-            if (currentClipboardContent !== null && currentClipboardContent !== undefined && currentClipboardContent !== '') {
-                if (text !== null)
-                    text.innerText = currentClipboardContent.length > maxSymbolsInBody ? currentClipboardContent.substring(0, maxSymbolsInBody - 3) + '...' : currentClipboardContent;
-                if (label !== null)
-                    label.innerHTML = chrome.i18n.getMessage('clipboard') + '<br />';
-
-                const dxToShow = realLeftCoord + (canvasRadius / 2) - (linkTooltip.clientWidth / 2);
-                const dyToShow = realTopCoord - (linkTooltip.clientHeight) - 8;
-                linkTooltip.style.transform = `translate(${dxToShow}px, ${dyToShow}px)`;
-            }
-
-        } else {
-            // text.innerText = configs.showFullLinkInTooltip ? linkToDisplay : linkToDisplay.length > maxSymbolsInBody ? linkToDisplay.substring(0, maxSymbolsInBody - 3) + '...' : linkToDisplay;
-            text.innerText = linkToDisplay;
-        }
+    /// Add type label
+    addTypeLabel(typeOfMenu, linkTooltip);
+    function addTypeLabel(type, parent){
+        const label = document.createElement('span');
+        label.setAttribute('class', 'cmg-helper-tooltip-label');
+        label.innerText = type == 'linkMenu' ?
+            // configs.showLinkTextInTooltip && hoveredLinkTitle !== null && hoveredLinkTitle !== undefined && hoveredLinkTitle !== ''
+            //     && hoveredLinkTitle !== hoveredLink && hoveredLinkTitle !== linkToDisplay
+            //     ? (hoveredLinkTitle.length > 24 ? hoveredLinkTitle.substring(0, 24) + '...' : hoveredLinkTitle)
+            //     : 
+                chrome.i18n.getMessage('link')
+            : (type == 'imageMenu' || type == 'imageLinkMenu') ? chrome.i18n.getMessage('image')
+                : typeOfMenu == 'playerMenu' ? chrome.i18n.getMessage('player')
+                    : type == 'selectionMenu' || (type == 'textFieldMenu' && textSelection.toString() !== '') ? chrome.i18n.getMessage('selectedText') :
+                    type == 'textFieldMenu' ? chrome.i18n.getMessage('inputField') : chrome.i18n.getMessage('page');
+    
+        label.innerHTML += '<br />';
+        label.style.color = `rgba(${fgColorRgb.red}, ${fgColorRgb.green}, ${fgColorRgb.blue}, 0.85)`;
+        parent.appendChild(label);
     }
 
-    text.setAttribute('style', `
-color: rgba(${fgColorRgb.red}, ${fgColorRgb.green}, ${fgColorRgb.blue}, 1.0);
-word-break:break-all;
-overflow: hidden !important;
-text-overflow: ellipsis !important;
-overflow: hidden !important;
-display: -webkit-box;
--webkit-box-orient: vertical;
--webkit-line-clamp: 2;
-`);
-    linkTooltip.appendChild(text);
+    /// Set body
+    addContents(typeOfMenu, linkTooltip)
+    async function addContents(typeOfMenu, linkTooltip){
+        const text = document.createElement('span');
+        text.id = 'cmg-helper-tooltip-body';
+        const maxSymbolsInBody = 33;
+
+        if (typeOfMenu == 'imageMenu' || typeOfMenu == 'playerMenu' || typeOfMenu == 'imageLinkMenu') {
+            let fileName;
+            let link = typeOfMenu == 'imageLinkMenu' ? hoveredImageLink : hoveredLink;
+            try {
+                fileName = link.split('#').shift().split('?').shift().split('/').pop();
+            } catch (e) { if (configs.debugMode) console.log(e); }
+            text.innerText = fileName && fileName.includes('.') ? fileName : link;
+        } else {
+            if (typeOfMenu == 'selectionMenu' || (typeOfMenu == 'textFieldMenu' && textSelection.toString() !== '')) {
+                const textSelectionString = textSelection.toString();
+                // text.innerText = textSelectionString;
+                text.innerText = `${textSelectionString.length} ${chrome.i18n.getMessage('symbolsCount').toLowerCase()}, ${textSelectionString.split(' ').length} ${chrome.i18n.getMessage('wordsCount').toLowerCase()}`;
+            } else if (typeOfMenu == 'textFieldMenu') {
+
+                /// Set tooltip body to display current clipboard content
+                const result = await getCurrentClipboard();
+                currentClipboardContent = result;
+
+                if (configs.debugMode) {
+                    console.log('current clipboard content:');
+                    console.log(currentClipboardContent);
+                }
+
+                if (currentClipboardContent !== null && currentClipboardContent !== undefined && currentClipboardContent !== '') {
+                    if (text !== null)
+                        text.innerText = currentClipboardContent.length > maxSymbolsInBody ? currentClipboardContent.substring(0, maxSymbolsInBody - 3) + '...' : currentClipboardContent;
+                    if (label !== null)
+                        label.innerHTML = chrome.i18n.getMessage('clipboard') + '<br />';
+
+                    const dxToShow = realLeftCoord + (canvasRadius / 2) - (linkTooltip.clientWidth / 2);
+                    const dyToShow = realTopCoord - (linkTooltip.clientHeight) - 8;
+                    linkTooltip.style.transform = `translate(${dxToShow}px, ${dyToShow}px)`;
+                }
+
+            } else {
+                // text.innerText = configs.showFullLinkInTooltip ? linkToDisplay : linkToDisplay.length > maxSymbolsInBody ? linkToDisplay.substring(0, maxSymbolsInBody - 3) + '...' : linkToDisplay;
+                text.innerText = linkToDisplay;
+            }
+        }
+
+        text.setAttribute('style', `
+            color: rgba(${fgColorRgb.red}, ${fgColorRgb.green}, ${fgColorRgb.blue}, 1.0);
+            word-break:break-all;
+            overflow: hidden !important;
+            text-overflow: ellipsis !important;
+            overflow: hidden !important;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+            `);
+        linkTooltip.appendChild(text);
+    }
+
+
+    /// Add second line for link+image menu
+    if (typeOfMenu == 'imageLinkMenu'){
+        // text.innerHTML += '<br>';
+
+        addTypeIcon('linkMenu', linkTooltip)
+        addTypeLabel('linkMenu', linkTooltip);
+        addContents('linkMenu', linkTooltip)
+    }
+
+    
     document.body.appendChild(linkTooltip);
 
     const linkTooltipHeight = linkTooltip.clientHeight;
